0b042489f57b328ee53af7e90c7aceb1aea7d95a
mansibavliya <109659593+mansibavliya@users.noreply.github.com>
2022-12-21 09:55:15 +0530

    project

---
+++ project/Responsive navbar/navbar.js
@@ -0,0 +1,9 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
+
+
